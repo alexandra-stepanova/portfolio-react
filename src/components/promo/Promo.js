819950ec2485335ef35657c5915887b5ea6feb44
Sasha Stepanova <chernishevskie@yandex.ru>
2022-03-02 14:34:43 +0200

    feat: added Promo component

---
+++ src/components/promo/Promo.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+function Promo() {
+  useEffect(() => {
+    Aos.init({ duration: 100 });
+    Aos.refresh();
+  }, []);
+
+  return (
+    <section className="promo">
+      <div className="promo__container">
+        <h1
+          className="title"
+          data-aos="fade-up"
+          data-aos-delay="10"
+          data-aos-duration="100"
+          data-aos-easing="ease-in-out"
+        >
+          <span className="promo__span">Hi. I'm Sasha.</span>
+          <span className="promo__span">A junior Frontend developer.</span>
+        </h1>
+        <p
+          className="subtitle"
+          data-aos="fade-up"
+          data-aos-delay="20"
+          data-aos-duration="200"
+          data-aos-easing="ease-in-out"
+        >
+          Currently, I'm a freelance developer looking to land a permanent job.
+          Meanwhile, I'm developing a few small projects and continue learning Redux, VUE and UI/UX.
+        </p>
+      </div>
+      <div className="promo__image"></div>
+    </section>
+  );
+}
+
+export default Promo;
