2b438e04225bebfe1193f680762b5fabbcfe6775
Sasha Stepanova <chernishevskie@yandex.ru>
2022-03-02 14:33:39 +0200

    feat: added Main component

---
+++ src/components/main/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import MediaQuery from "react-responsive";
+import Promo from "../promo/Promo";
+import Profile from "../profile/Profile";
+import Projects from "../projects/Projects";
+import Contact from "../contact/Contact";
+import Skills from "../skills/Skills";
+
+function Main() {
+  return (
+    <main className="main">
+      <Promo />
+      <Profile />
+      <MediaQuery maxWidth={999}>
+        <Skills />
+      </MediaQuery>
+      <Projects />
+      <Contact />
+    </main>
+  );
+}
+
+export default Main;
