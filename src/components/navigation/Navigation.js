5e798b58be9e30bf4ca7429c672130f08e41c68a
Sasha Stepanova <chernishevskie@yandex.ru>
2022-05-09 23:28:14 +0300

    refactor: uses prettier

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,49 +1,47 @@
 import React from "react";
 import { Link } from "react-scroll";
 
-function Navigation(props) {
-
+function Navigation({ onClose }) {
   return (
     <ul className="navigation">
       <li className="margin-btn">
         <Link
-        className="link margin-btn"
-        activeClass="link-underline"
+          onClick={onClose}
+          className="link margin-btn"
+          activeClass="link-underline"
           to="profile"
           spy={true}
           smooth={true}
           offset={-100}
           duration={500}
-          onClick={props.onClose}
         >
           About
         </Link>
       </li>
       <li className="margin-btn">
         <Link
-        className="link"
-        activeClass="link-underline"
+          className="link"
+          activeClass="link-underline"
           to="projects"
           spy={true}
           smooth={true}
           offset={-60}
           duration={500}
-          onClick={props.onClose}
+          onClick={onClose}
         >
           Projects
         </Link>
       </li>
-      <li
-      >
-         <Link
-        className="link"
-        activeClass="link-underline"
+      <li>
+        <Link
+          className="link"
+          activeClass="link-underline"
           to="contact"
           spy={true}
           smooth={true}
           offset={-30}
           duration={500}
-          onClick={props.onClose}
+          onClick={onClose}
         >
           Contact Me
         </Link>
